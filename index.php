b5523f384f1c4a10781837669be278726a988233
Feiv7 <benjamingoncalves@hotmail.fr>
2020-03-05 17:51:14 +0100

    Modification index et autre page + css rem

--- index.php
+++ index.php
@@ -7,35 +7,35 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php print titre ?></title>
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="description">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-	
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-	<ul>
-		<li><a href="index.php?page=1">&lt;/&gt;  L'école</a></li>
-		<li><a href="index.php?page=2">&lt;/&gt; Formations</a></li>
-		<li><a href="index.php?page=3">&lt;/&gt;  Contact</a></li>
+	<header>
+		<ul>
+			<li><a href="index.php?page=1">&lt;/&gt;  L'école</a></li>
+			<li><a href="index.php?page=2">&lt;/&gt; Formations</a></li>
+			<li><a href="index.php?page=3">&lt;/&gt;  Contact</a></li>
 		
-	</ul>
+		</ul>
+	</header>
 	<main>
 		<h1><?php print titre ?></h1>
-
 	
 			<?php
 				if(isset($_GET['page'])){
 				//variable pour $_GET
 				$root = $_GET['page'];
 						if($root==1){
-							include_once("./template/index.html");
+							include_once("./template/index.inc.php");
 						}
 						if($root==2){
-							include_once("./template/portfolio.html");
+							include_once("./template/portfolio.inc.php");
 						}
 						if($root==3){
-							include_once("./template/contact.html");
+							include_once("./template/contact.inc.php");
 						}
 						else if($root>3 || $root==0){
 							echo '<p class="warning"><i class="far fa-frown"></i> La page demandée n\'existe pas!!</p>';
@@ -44,10 +44,10 @@
 
 				}
 					else{
-						include("./template/index.html");
+						include("./template/index.inc.php");
 					}
 
 			?>
-	</div>
+	</main>
 </body>
 </html>
---
+++ template/footer.inc.php
@@ -0,0 +1,3 @@
+<?php
+echo "<footer><p>&copy; - EDW - 2020</p></footer>";
+?>
