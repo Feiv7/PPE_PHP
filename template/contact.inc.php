173e5daf3d47c2400675f905c3b1f133e7b19fce
Feiv7 <benjamingoncalves@hotmail.fr>
2020-03-12 11:30:37 +0100

    contact champ texte --3

--- template/contact.inc.php
+++ template/contact.inc.php
@@ -11,7 +11,7 @@
 			<label for="prenom">Votre Mail</label>
 			<input type="email" name="mail" id="prenom" placeholder="mail" required aria-required="true">
 			<label for="prenom">Votre message</label>
-			<textarea name="commentaire" id="" cols="30" rows="10"></textarea>
+			<input type="text" name="commentaire" id="commentaire" placeholder="commentaire" required aria-required="true">
 			<input type="submit" value="Envoyer">
 			
 		</form>
