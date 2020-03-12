e7f505acdfd4d04a0071a2549c62223bef277ca8
Feiv7 <benjamingoncalves@hotmail.fr>
2020-03-12 12:45:29 +0100

    contact input4

--- template/contact.inc.php
+++ template/contact.inc.php
@@ -2,20 +2,18 @@
 <section>
 	<h2>Contact</h2>
 		<form action="./contact.php" method="post">
-			<!--<label for="Cursus">Cursus</label>-->
 			<label for="Cursus"></label>
 			<input type="text" name="Cursus" id="Cursus" placeholder="Cursus">
-			<!--<label for="nom">Nom</label>-->
+			<label for="nom"></label>
 			<input type="text" name="nom" id="nom" placeholder="nom" required aria-required="true" autofocus>
-
-			<!--<label for="prenom">Prénom</label>-->
-			<input type="text" name="nom" id="prenom" placeholder="prénom" required aria-required="true">
-
-			<!--<label for="prenom">Email</label>-->
+			<label for="prenom"></label>
+			<input type="text" name="prenom" id="prenom" placeholder="prénom" required aria-required="true">
+			<label for="mail"></label>
 			<input type="email" name="mail" id="mail" placeholder="mail" required aria-required="true">
-			<!--<label for="prenom">Votre message</label>-->
-			<input type="text" name="telephone" id="telephone" placeholder="Telephone">
-			<input type="text" name="commentaire" id="commentaire" placeholder="niveau d'etudes actuel" >
+			<label for="telephone"></label>
+			<input type="text" name="telephone" id="telephone" placeholder="Telephone" required aria-required="true">
+			<label for="commentaire"></label>
+			<input type="text" name="commentaire" id="commentaire" placeholder="niveau d'etudes actuel" required aria-required="true">
 			<input type="submit" value="Envoyer">
 			
 		</form>
